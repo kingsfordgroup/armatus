7af75c65b4d6e58edc3e61700939d246b4bc8d8f
Darya Filippova <lynxoid@macLynxoid.local>
2013-07-01 16:09:17 -0400

    src/Armatus.cpp

--- src/Armatus.cpp
+++ src/Armatus.cpp
@@ -1,5 +1,16 @@
 #include "Version.hpp"
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
+
+  cout << "***********************************" << endl;
+  cout << "***********************************" << endl;
+  cout << "****                           ****" << endl;
+  cout << "****        ARMATUS 1.0        ****" << endl;
+  cout << "****                           ****" << endl;
+  cout << "***********************************" << endl;
+  cout << "***********************************" << endl;
   return 0;
 }
