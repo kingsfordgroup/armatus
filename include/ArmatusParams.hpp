7f4f96579bc17df2f627961a62c28b8fb9cd2f4c
Rob Patro <rob.patro@gmail.com>
2013-07-03 17:03:21 -0400

    pre-merge

--- include/ArmatusParams.hpp
+++ include/ArmatusParams.hpp
@@ -11,16 +11,12 @@ class ArmatusParams {
   using SymmetricMatrix = boost::numeric::ublas::symmetric_matrix<double, boost::numeric::ublas::upper>;
   public:
     explicit ArmatusParams(std::shared_ptr<MatrixParser::SparseMatrix> _A, double _gamma);
-
+    void computeSumMuSigma_();
     std::shared_ptr<MatrixParser::SparseMatrix> A;
     SymmetricMatrix sums;
-    std::vector<double> mu;
-    std::vector<double> sigma;
-    std::vector<double> median;
-    std::vector<uint32_t> numSamps;
-    double qmax;
-    double gamma;
     size_t n;
+    double gamma;
+    std::vector<double> mu;
 };
 
 #endif // __ARMATUS_PARAMS_HPP__
